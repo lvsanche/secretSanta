6f7ee9a5cb2c372e40ae39ca3d0359d24a4c2d63
LUIS SANCHEZ <lvsanche@ucsd.edu>
2016-11-20 16:08:05 -0800

    So far so good, still need the cross senders

--- python/send.py
+++ python/send.py
@@ -5,24 +5,37 @@ from random import shuffle
 import math
 
 
-def send_message(user, pw, rec, subject, body):
+def send_message(user, pw, rec, body):
     gmail_user = user
     gmail_pwd = pw
     FROM = user
     TO = rec if type(rec) is list else [rec]
-    SUBJECT = subject
+    SUBJECT = ""
     TEXT = body
-    
-    # Prepare actual message
-    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
-    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
-    
-    
+    #time to see if body is < 150
     server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
     server_ssl.login(gmail_user, gmail_pwd)  
-    # ssl server doesn't support or need tls, so don't call server_ssl.starttls() 
-    server_ssl.sendmail(FROM, TO, message)
-    #server_ssl.quit()
+    
+    if len(TEXT) >=150:
+        while len(TEXT) >= 150:
+            tosend = TEXT[:150]
+            message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+            """ % (FROM, ", ".join(TO), SUBJECT, tosend)    
+            server_ssl.sendmail(FROM, TO, message)
+            TEXT=TEXT[150:len(TEXT)]
+            if len(TEXT) <=150:
+                tosend = TEXT[:len(TEXT)]
+                message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+                """ % (FROM, ", ".join(TO), SUBJECT, tosend)    
+                server_ssl.sendmail(FROM, TO, message)
+    else:
+    # Prepare actual message
+        message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+        """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+        # ssl server doesn't support or need tls, so don't call server_ssl.starttls() 
+        server_ssl.sendmail(FROM, TO, message)
+    
+    
     server_ssl.close()
     print 'successfully sent the mail'
 
@@ -103,12 +116,48 @@ def readSantas(listPeople):
     santas = []
     for line in ppl:
         line = line[:len(line)-1]
-        array = line.split(" ")
+        array = line.split(",")
         name = array.pop(0)
         number = array.pop(0)
         wshlist = copy.deepcopy(array)
         santas.append(Santas(name,number,wshlist))
     return santas
+
+def completeRun(fileName, sender, pw):
+    print "STARTING TO IMPORT"
+    santas = readSantas(fileName)
+    completed = Santas.randomizeList(santas)
+    if completed == False:
+        print Failed
+    elif Santas.santasDone(completed):
+        #send out the messages
+        print "ABOUT TO START SENDING OUT MESSAGES"
+        for s in completed:
+            print "Santa: %s , send: %s, %s , rec : %s , %s \n"  %(s.name, s.send_to[0], s.send_to[1], s.rec_from[0], s.rec_from[1])
+        
+        for santa in completed:
+            send_message(sender, pw, santa.number, "You are %s's and %s's Secret Santa!" %(santa.send_to[0], santa.send_to[1]));
+                        
+            for receiver in completed:
+                if receiver.name == santa.send_to[0]:
+                    wishlis = "%s's wishlist:  " %santa.send_to[0]
+                    for thing in receiver.wshlist:
+                        wishlis+= "%s, " %thing
+                    wishlis=wishlis[:len(wishlis)-2]
+                    print receiver.name
+                    print wishlis
+                    send_message(sender, pw, santa.number, wishlis)
+
+                elif receiver.name == santa.send_to[1]:
+                    wishlis = "%s's wishlist:  " %santa.send_to[1]
+                    for thing in receiver.wshlist:
+                        wishlis+= "%s, " %thing
+                    wishlis=wishlis[:len(wishlis)-2]
+                    print receiver.name
+                    print wishlis
+                    send_message(sender, pw, santa.number, wishlis)
+                
+
 
 def Run():
     print "STAR"
